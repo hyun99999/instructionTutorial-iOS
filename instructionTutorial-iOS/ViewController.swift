bfd62ae59e3f6e23297124328024bf8ef312baf7
hyungyu Kim <ypsophila97@icloud.com>
2021-03-07 22:48:45 +0900

    Implement Main.storyboard

--- instructionTutorial-iOS/ViewController.swift
+++ instructionTutorial-iOS/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import Instructions
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var profileLabel: UILabel!
+    @IBOutlet weak var subscribeBtn: UIButton!
+    @IBOutlet weak var alartmBtn: UIButton!
+    @IBOutlet weak var likeBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
